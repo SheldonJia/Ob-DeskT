20681d2220ddb2ffa967b3067ac4bb52a11e60dd
sheldonjia <317301424@qq.com>
2019-03-20 23:55:09 +0800

    1.0.0.1

--- core/log/log.temp.php
+++ core/log/log.temp.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * @BY  : phpStorm
+ * @Name: GogoPro
+ * @Date: 2019/3/20
+ * @Time: 20:32
+ * @Remarks: 
+ * @Version: 1.0.0.0 主版本号.子版本号.修订版本号.编译版本号
+ */
+
 //定义命名空间
 namespace ob_deskt\core\Log;
 /*
