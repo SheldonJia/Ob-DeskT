a73398faec20a8d9e669d05f210bbadeaf86a09e
sheldonjia <317301424@qq.com>
2019-03-20 20:44:53 +0800

    1.0.0.1

---
+++ core/view.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @BY  : phpStorm
+ * @Name: GogoPro
+ * @Date: 2019/3/20
+ * @Time: 20:34
+ * @Remarks: 视图控制器
+ * @Version: 1.0.0.0 主版本号.子版本号.修订版本号.编译版本号
+ */
+
+/**
+ * @Name(开发者): GogoPro
+ * @DateTime(开发创建时间): 2019/3/20 20:34
+ * @LastTime(最后修改时间): 2019/3/20 20:34
+ * @Remarks(备注): 视图控制器
+ * @final(不可继承): 否
+ */
+class V
+{
+    //初始化函数
+    function __construct()
+    {
+    }
+
+    //析构函数
+    function destruct()
+    {
+    }
+
+    /**
+     * @Name(开发者): GogoPro @DateTime(开发创建时间): 2019/3/20 20:34 @LastTime(最后修改时间): 2019/3/20 20:34
+     * @type(类型):public(公开)//protected(保护)//private(私有) @final(不可继承):否 @static(静态):否
+     * @parameter1(参数1):类型，默认值，说明
+     * @parameter2(参数2):类型，默认值，说明
+     * @parameterN(参数N):类型，默认值，说明
+     * @return(返回):类型，说明
+     * @Remarks(备注):文件说明
+     */
+    public function functionName()
+    {
+    }
+}
+
+?>
+ 
